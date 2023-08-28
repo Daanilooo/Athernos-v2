a495b38a4f8f10323f04332f7ff5fbea3cd5d48e
Danilo Souza <daanilosouza273@gmail.com>
2023-08-27 22:23:35 -0300

    entrada de lotes e começo da tela de usuarios

---
+++ src/views/telas/listaUsuario.php
@@ -0,0 +1,4 @@
+<?php
+ include_once $_SERVER['DOCUMENT_ROOT'].'/Athernos/src/views/telas/menu-lateral.php'; 
+
+?>
--- src/views/telas/menu-lateral.php
+++ src/views/telas/menu-lateral.php
@@ -39,7 +39,7 @@
                             <svg style="display:none;" class="svg-baixo" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><path d="M18.71,8.21a1,1,0,0,0-1.42,0l-4.58,4.58a1,1,0,0,1-1.42,0L6.71,8.21a1,1,0,0,0-1.42,0,1,1,0,0,0,0,1.41l4.59,4.59a3,3,0,0,0,4.24,0l4.59-4.59A1,1,0,0,0,18.71,8.21Z"/></svg>
                         </span>
                         <div class="nav-bar__itens hide">
-                            <a class="nav-bar__item" href='#'>Listar Usuários</a>
+                            <a class="nav-bar__item" href='Athernos/src/views/telas/listaUsuario.php'>Listar Usuários</a>
                             <a class="nav-bar__item" href='#'>Cadastrar Usuario</a>
                         </div>
                     <!-- <?php //} ?> -->
